a8402f3f034342724072f197c822ae35628cc681
pradip <p.pack@comicreleif.com>
2018-07-02 16:59:16 +0100

    Refactor

--- src/test/java/utils/RestServices.java
+++ src/test/java/utils/RestServices.java
@@ -2,9 +2,12 @@ package utils;
 
 import java.util.Properties;
 
+import fixture.PayLoads;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+
+import static io.restassured.RestAssured.given;
 
 public class RestServices {
 	
@@ -14,38 +17,45 @@ public class RestServices {
 		
 		prop = Helper.loadPropertyFile("./src/test/resources/endpoints.properties");
 	}
-
+	
+	// Create new account
 	public static Response postAccount(RequestSpecification reqSpec, String payload) {
-		
-		return reqSpec.body(payload).log().all().when().post(prop.getProperty("CREATE-ACCT"));
+				
+		return given().spec(reqSpec).body(payload).when().post(prop.getProperty("CREATE-ACCT"));
 	}
-
+	
+	// Authenticate user
 	public static Response postAuthenticate(RequestSpecification reqSpec, String payload) {
 
-		return reqSpec.body(payload).when().post(prop.getProperty("AUTHENTICATE"));
+		return given().spec(reqSpec).body(payload).when().post(prop.getProperty("AUTHENTICATE"));
 	}
-
+	
+	// Check email exists or not
 	public static Response getEmail(RequestSpecification reqSpec, String emailAddress) {
 		
-		String emailEndpoint = prop.getProperty("EMAIL") + "/"+ emailAddress;
-		return reqSpec.when().get(emailEndpoint);
+		return given().spec(reqSpec).pathParam("email-id", emailAddress).when().get(prop.getProperty("EMAIL-ID"));
 	}
-
+	
+	// Get user data with user id and token
 	public static Response getUser(RequestSpecification reqSpec, String token, int userId) {
 
-		String userIdEndpoint = prop.getProperty("CREATE-ACCT") + "/"+ userId;
-		return reqSpec.header("Authorization",token).when().get(userIdEndpoint);
+		return given().spec(reqSpec).header("Authorization",token).pathParam("user-id", userId).when().get( prop.getProperty("USER-ID"));
 	}
-
-	public static String postAuthenticateAndRetrieveToken(RequestSpecification reqSpec, String payload) {
+	
+	// Generate JWS token using existing email and password
+	public static String generateJWSToken(RequestSpecification reqSpec, String email, String password) {
 		
-		Response res = reqSpec.body(payload).when().post(prop.getProperty("AUTHENTICATE"));
+		PayLoads pl = new PayLoads();
+		pl.setEmailAndPasswordPayload(email, password);
+		String payload = pl.getEmailAndPasswordPayload();
+		
+		Response res = given().spec(reqSpec).body(payload).when().post(prop.getProperty("AUTHENTICATE"));
 		JsonPath jp = new JsonPath(res.asString());
 		String payl = jp.get("payload");
 		String protect = jp.get("protected");
 		String signature = jp.get("signature");
-		String jwt_token = "Bearer " + protect + "." + payl + "." + signature;
-		return jwt_token;
+		String jws_token = "Bearer " + protect + "." + payl + "." + signature;
+		return jws_token;
 	}
 
 }
